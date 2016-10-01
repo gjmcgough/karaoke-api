c5f5992e5f55b51fe326a4806c9834a03421f40e
Alec & Greg McGough <pair+agoeckel.gjmcgough@devbootcamp.com>
2016-09-30 18:02:26 -0700

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  root 'users#show'
+
+  get 'users/show'
+  get 'users/new'
+  post 'users'
+  get 'users/:id/edit'
+  put 'users'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#show'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
