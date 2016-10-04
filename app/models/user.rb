e5c1e7506b9e997f56d9ec3f8c874a42f3c4a0e3
David Thomas & Kathryn Ragudos <pair+kawikathomas.kragudos19@devbootcamp.com>
2016-10-03 18:36:03 -0700

    Deploy for nate.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth_hash)
     where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create do |user|
+      p user
       user.provider = auth_hash.provider
       user.uid = auth_hash.uid
       user.name = auth_hash.info.name
