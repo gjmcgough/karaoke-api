c69d0b2d93c7351e31cfea164d7439b4f1ddf7e2
David Thomas & Kathryn Ragudos <pair+kawikathomas.kragudos19@devbootcamp.com>
2016-10-03 17:23:46 -0700

    Add spotify search.

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,19 +1,26 @@
 class Api::SongsController < ApplicationController
-  before_filter :find_song
+  # before_filter :find_song
+
+  # def create
+  #   if @song.present?
+  #     render nothing: true # no error is rendered
+  #   else
+  #     @song = Song.new
+  #     @song.attributes(title: @json['title'], artist: @json['artist'])
+  #   end
+  # end
 
   def create
-    if @song.present?
-      render nothing: true # no error is rendered
-    else
-      @song = Song.new
-      @song.attributes(title: @json['title'], artist: @json['artist'])
-    end
+    song_params = params[:song]
+    @song = Song.create(title: song_params[:title], artist: song_params[:artist_name])
+    PlaylistSong.create(playlist_id: current_user.playlist.id, song_id: @song.id)
+    redirect_to api_artist_path(song_params[:artist_id])
   end
 
   private
-  def find_song
-    @song = Song.find_by(title: @json['title'])
-    head 409 unless @song.present?
-  end
+  # def find_song
+  #   @song = Song.find_by(title: @json['title'])
+  #   head 409 unless @song.present?
+  # end
 
 end
